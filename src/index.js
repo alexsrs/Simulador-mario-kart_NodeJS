6b00d2e992b0d6dcf29e5cf2a370629b5fc109ec
Alex Sandro <alex@creapix.com.br>
2025-03-20 15:35:21 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+}
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+}
+
+const player3 = {
+    NOME: "Peach",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+    PONTOS: 0,
+}
+
+const player4 = {
+    NOME: "Yoshi",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+}
+
+const player5 = {
+    NOME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+}
+
+const player6 = {
+    NOME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+}
+
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+(async function main() {
+    let dice = await rollDice();
+    console.log(dice);
+})() // função auto invocada não precisa de chamar a função
+// main();
+
